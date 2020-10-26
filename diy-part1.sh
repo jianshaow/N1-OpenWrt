87126ba02a16e6e2fd2934d515730394d0a30d7e
Jianshao Wu <787903+jianshaow@users.noreply.github.com>
2020-10-26 20:13:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,9 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git lienol https://github.com/xiaorouji/openwrt-package' feeds.conf.default
+
+git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash
